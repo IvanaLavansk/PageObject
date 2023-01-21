c4a32f28fd6610a374753548b7dd14f4fb4afe4c
Marat Field <mrpolyansky@yandex.ru>
2023-01-21 11:07:51 +0300

    PageObject сÑ code source

--- src/test/java/ru/netology/pageobject/test/MoneyTransferTest.java
+++ src/test/java/ru/netology/pageobject/test/MoneyTransferTest.java
@@ -1,4 +1,42 @@
 package ru.netology.pageobject.test;
 
-public class MoneyTransferTest {
+import org.junit.jupiter.api.Test;
+import ru.netology.pageobject.data.DataHelper;
+import ru.netology.pageobject.page.LoginPageV1;
+import ru.netology.pageobject.page.LoginPageV2;
+import ru.netology.pageobject.page.LoginPageV3;
+
+import static com.codeborne.selenide.Selenide.open;
+
+class MoneyTransferTest {
+    @Test
+    void shouldTransferMoneyBetweenOwnCardsV1() {
+        open("http://localhost:9999");
+        var loginPage = new LoginPageV1();
+//    var loginPage = open("http://localhost:9999", LoginPageV1.class);
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+    }
+
+    @Test
+    void shouldTransferMoneyBetweenOwnCardsV2() {
+        open("http://localhost:9999");
+        var loginPage = new LoginPageV2();
+//    var loginPage = open("http://localhost:9999", LoginPageV2.class);
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+    }
+
+    @Test
+    void shouldTransferMoneyBetweenOwnCardsV3() {
+        var loginPage = open("http://localhost:9999", LoginPageV3.class);
+        var authInfo = DataHelper.getAuthInfo();
+        var verificationPage = loginPage.validLogin(authInfo);
+        var verificationCode = DataHelper.getVerificationCodeFor(authInfo);
+        verificationPage.validVerify(verificationCode);
+    }
 }
